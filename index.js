5ef6e9f06d57c8770f847d3668832e20b675000b
AGpeter2018 <adenijipeter2018@gmail.com>
2025-07-27 13:32:09 +0100

    added the dark and light mode functionality to the project and an adjustment in the colour of the nav bar

--- index.js
+++ index.js
@@ -23,3 +23,15 @@ arrow.forEach((item, index) => {
   });
   console.log(moviesList[index].querySelectorAll("img").length);
 });
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container, .movie-list-title, .navbar-container, .menu-item a, .profile-container, .sidebar, .left-menu-icon, .toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
